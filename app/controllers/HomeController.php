25b859267e76feb1a4ffe286033ab8f4c0b5c6bb
blankon123 <thosan.girisona@gmail.com>
2015-08-14 01:10:23 +0700

    Commit Tombol profile-kontes
    hapus tombol, ganti tabel kondisional

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -41,108 +41,23 @@ class HomeController extends BaseController {
 
 	public function showProfile()
 	{
+		$dataKontes = array();
 		$data = Auth::user()->anggota()->orderBy('urutan')->get();
-		
-		return View::make('user.profile')->with('data',$data);
+		$KontCount = Kontes::all()->count();
+		$j = 0;
+		for ($i=0; $i <$KontCount; $i++) { 
+			if (Auth::user()->registered($i) ) {
+				$dataKon = Kontes::find($i);
+				if(strtotime($dataKon->endtime) > strtotime("now"))
+					$dataKontes[$j++] = $dataKon;
+			}
+		}
+		return View::make('user.profile')->with('data',$data)->with('dataKontes',$dataKontes);
 	}
 
 	public function getEditProfile(){
         $data = Auth::user()->anggota()->orderBy('urutan')->get();
         return View::make('user/edit')->with('data',$data);
-    }
-
-    public function postEditProfile(){
-    	$rules = array(
-            'kontak1' => 'between:10,12',
-            'kontak2' => 'between:10,12',
-            //'kontak3' => 'between:10,12',
-            'tahunmasuk1' => 'between:4,4',
-            'tahunmasuk2' => 'between:4,4',
-            //'tahunmasuk3' => 'between:4,4',
-            'kp1' 		=> 'image|max:5000',
-            'kp2'		=> 'image|max:5000'
-            //'kp3'		=> 'image|max:5000'
-        );
-
-    	$validator = Validator::make(Input::all(), $rules);
-
-    	if ($validator->fails()) {
-
-	        // get the error messages from the validator
-	        $messages = $validator->messages();
-
-	        // redirect our user back to the form with the errors from the validator
-	        return Redirect::to('edit-profile')->withErrors($validator);
-
-	    } else {
-	        $user1 = Anggota::find(Input::get('idag1'));
-	        $user2 = Anggota::find(Input::get('idag2'));
-	        //$user3 = Anggota::find(Input::get('idag3'));
-	        
-	        if(Input::get('nama1')!=null) $user1->nama = Input::get('nama1');
-	        if(Input::get('nama2')!=null) $user2->nama = Input::get('nama2');
-	        //if(Input::get('nama3')!=null) $user3->nama = Input::get('nama3');
-	        
-	        if(Input::get('nis1')!=null) $user1->nis = Input::get('nis1');
-	        if(Input::get('nis2')!=null) $user2->nis = Input::get('nis2');
-	        //if(Input::get('nis3')!=null) $user3->nis = Input::get('nis3');
-
-	        if(Input::get('tahunmasuk1')!=null) $user1->tahun_masuk = Input::get('tahunmasuk1');
-	        if(Input::get('tahunmasuk2')!=null) $user2->tahun_masuk = Input::get('tahunmasuk2');
-	        //if(Input::get('tahunmasuk3')!=null) $user3->tahun_masuk = Input::get('tahunmasuk3');
-
-	        if(Input::get('kontak1')!=null) $user1->handphone = Input::get('kontak1');
-	        if(Input::get('kontak2')!=null) $user2->handphone = Input::get('kontak2');
-	        //if(Input::get('kontak3')!=null) $user3->handphone = Input::get('kontak3');
-	    	
-	    	if(Input::file('kp1')!=null){
-                $destinationPath = 'assets/image/isco/sg/kp';
-                $file = Input::file('kp1');
-                $extension = Input::file('kp1')->getClientOriginalExtension();
-                $filename = '1'.sha1(time().time()).".{$extension}";
-
-                $upload_success = Input::file('kp1')->move($destinationPath, $filename);
-                if($upload_success){
-                    $user1->kartu_pelajar_dir = $destinationPath.'/1'.$filename;
-                }
-                else return Redirect::to('edit-profile')->with('errors','Terjadi Kesalahan, Coba Daftar Lagi');
-            }
-			
-			if(Input::file('kp2')!=null){
-                $destinationPath2 = 'assets/image/isco/sg/kp';
-                $file2 = Input::file('kp2');
-                $extension2 = Input::file('kp2')->getClientOriginalExtension();
-                $filename2 = '2'.sha1(time().time()).".{$extension2}";
-
-                $upload_success2 = Input::file('kp2')->move($destinationPath2, $filename2);
-                if($upload_success2){
-                    $user2->kartu_pelajar_dir = $destinationPath2.'/2'.$filename2;
-                }
-                else return Redirect::to('edit-profile')->with('errors','Terjadi Kesalahan, Coba Daftar Lagi');
-            }
-            
-            /*
-			
-			if(Input::file('kp3')!=null){
-                $destinationPath3 = 'assets/image/isco/sg/kp';
-                $file3 = Input::file('kp3');
-                $extension3 = Input::file('kp3')->getClientOriginalExtension();
-                $filename3 = sha1(time().time()).".{$extension3}";
-
-                $upload_success3 = Input::file('kp3')->move($destinationPath3, $filename3);
-                if($upload_success3){
-                    $user3->kartu_pelajar_dir = $destinationPath3.'/'.$filename3;
-                }
-                else return Redirect::to('edit-profile')->with('errors','Terjadi Kesalahan, Coba Daftar Lagi');
-            }
-            */
-
-			$user1->save();
-			$user2->save();
-			//$user3->save();
-			return Redirect::to('')->with('success','data anda berhasil di update');
-	    }
-
     }
 
 
--- app/views/user/profile.blade.php
+++ app/views/user/profile.blade.php
@@ -3,243 +3,283 @@
 @section('title', 'Lomba Statistician Game')
 
 @section('header')
-    @parent
-    {{ HTML::style('assets/css/pages/paper-form.css') }}
-    <style>
-      .info-maroon{
-        background-color:maroon;
-        color:white;
-      }
-    </style>
-  @stop
+@parent
+{{ HTML::style('assets/css/pages/paper-form.css') }}
+<style>
+	.info-maroon{
+		background-color:maroon;
+		color:white;
+	}
+</style>
+@stop
 
 @section('content')
-  <!-- BEGIN REGISTRATION FORM -->
-    
-  <div class="content2">
-    
+<!-- BEGIN REGISTRATION FORM -->
+
+<div class="content2">
 
 
-    <nav class="navbar navbar-default">
-      <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Statistician Game 2015</a>
-        </div>
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav navbar-right">
-            <a href="#"><button type="button" class="btn btn-default navbar-btn" disabled>Penyisihan</button></a>
-            <a href="logout"><button type="button" class="btn btn-default navbar-btn">Logout</button></a>
-          </ul>
-        </div><!-- /.navbar-collapse -->
-      </div><!-- /.container-fluid -->
-    </nav>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Statistician Game 2015</a>
+			</div>
 
-    @if(Session::has('error'))
-      <?php $error = Session::get('error'); ?>
-      <div class="alert alert-danger">
-        <button class="close" data-dismiss="alert"></button>
-        <span>{{$error}}</span>
-      </div>
-    @endif
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right">
+					<a href="logout"><button type="button" class="btn btn-default navbar-btn">Logout</button></a>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div><!-- /.container-fluid -->
+		@if(count($dataKontes)>0)
+		<hr>
+		<table class="table table-condensed text-center">
+			<thead>
+				<tr>
+					<th class="text-center">Kontes Aktif</th>
+					<th class="text-center">Waktu Tersisa</th>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td><h4><kbd>{{$dataKontes[0]->nama}}</kbd></h4></td>
+					<td id="timerUser"></td>
+					<td><a href="kontes/{{$dataKontes[0]->id}}"><button type="button" class="btn btn-default">Mulai</button></a></td>
+					
+				</tr>
+			</tbody>
+		</table>
+		@endif
+	</nav>
 
-    @if(Session::has('success'))
-      <?php $success = Session::get('success'); ?>
-      <div class="alert alert-success">
-        <button class="close" data-dismiss="alert"></button>
-        <span>{{$success}}</span>
-      </div>
-    @endif
 
-    <div class="alert alert-info">
-        <button class="close" data-dismiss="alert"></button>
-        <span>Harap segera melengkapi data tim anda, terima kasih</span>
-    </div>
+	@if(Session::has('error'))
+	<?php $error = Session::get('error'); ?>
+	<div class="alert alert-danger">
+		<button class="close" data-dismiss="alert"></button>
+		<span>{{$error}}</span>
+	</div>
+	@endif
 
-    <!-- END REGISTRATION FORM -->
-    <div id="#anggota">
-      <br>
-      <div class="row">
-        <div class="col-md-4">
-          <div class="panel panel-default">
-            <div class="panel-heading">Tim {{Auth::user()->username}}</div>
-            <div class="panel-body">
-               <table class="table table table-striped">
-                <tr>
-                  <td>E-Mail </td>
-                </tr>
-                <tr>
-                  <td class="pull-right">{{Auth::user()->email}}</td>
-                </tr>
+	@if(Session::has('success'))
+	<?php $success = Session::get('success'); ?>
+	<div class="alert alert-success">
+		<button class="close" data-dismiss="alert"></button>
+		<span>{{$success}}</span>
+	</div>
+	@endif
 
-                <tr>
-                  <td>Sekolah </td>
-                </tr>
-                <tr>
-                  <td class="pull-right">{{Auth::user()->asal_sekolah}}</td>
-                </tr>
-                <tr>
-                  <td>Status </td>
-                </tr>
-                <tr>
-                  @if(Auth::user()->status)
-                  <td class="pull-right">Terverifikasi</td>
-                  @else
-                  <td class="pull-right info-maroon">Belum Terverifikasi</td>
-                  @endif
-                </tr>
-              </table>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-8">
-          <!--Ketua Tim-->
-          <div class="panel panel-default">
-            <div class="panel-heading">Identitas Ketua Tim :</div>
-            <div class="panel-body">
-              @if(isset($data[0]))
-              <table class="table">
-                <tr>
-                  <td class="col-md-3">Nama </td>
-                  @if($data[0]->nama!=null)
-                  <td>{{$data[0]->nama}}</td>
-                  @else
-                  <td class="info-maroon">belum terisi</td>
-                  @endif
-                </tr>
-                <tr>
-                  <td>NIS </td>
-                  @if($data[0]->nis!=null)
-                  <td>{{$data[0]->nis}}</td>
-                  @else
-                  <td class="info-maroon">belum terisi</td>
-                  @endif
-                </tr>
-                <tr>
-                  <td>Tahun Masuk </td>
-                  @if($data[0]->tahun_masuk!=null)
-                  <td>{{$data[0]->tahun_masuk}}</td>
-                  @else
-                  <td class="info-maroon">belum terisi</td>
-                  @endif
-                </tr>
-                 <tr>
-                  <td>Kontak </td>
-                  @if($data[0]->handphone!=null)
-                  <td>{{$data[0]->handphone}}</td>
-                  @else
-                  <td class="info-maroon">belum terisi</td>
-                  @endif
-                </tr>
-                 <tr>
-                  <td>Kartu Pelajar </td>
-                  @if($data[0]->kartu_pelajar_dir!=null)
-                  <td>sudah terupload</td>
-                  @else
-                  <td class="info-maroon">belum terisi</td>
-                  @endif
-                </tr>
-                <tr>
-                  <td colspan=2 style="text-align:right"><a href="edit-profile">Edit/Update</a></td>
-                </tr>
-              </table>
-              @endif
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-        </div>
-        <div class="col-md-8">
-          <!--Ketua Tim-->
-          <div class="panel panel-default">
-            <div class="panel-heading">Identitas Anggota :</div>
-            <div class="panel-body">
-              <!-- Table -->
-              @if(isset($data[1]))
-              <table class="table">
-                <tr>
-                  <td class="col-md-3">Nama </td>
-                  @if($data[1]->nama!=null)
-                  <td>{{$data[1]->nama}}</td>
-                  @else
-                  <td class="info-maroon">belum terisi</td>
-                  @endif
-                </tr>
-                <tr>
-                  <td>NIS </td>
-                  @if($data[1]->nis!=null)
-                  <td>{{$data[1]->nis}}</td>
-                  @else
-                  <td class="info-maroon">belum terisi</td>
-                  @endif
-                </tr>
-                <tr>
-                  <td>Tahun Masuk </td>
-                  @if($data[1]->tahun_masuk!=null)
-                  <td>{{$data[1]->tahun_masuk}}</td>
-                  @else
-                  <td class="info-maroon">belum terisi</td>
-                  @endif
-                </tr>
-                 <tr>
-                  <td>Kontak </td>
-                  @if($data[1]->handphone!=null)
-                  <td>{{$data[1]->handphone}}</td>
-                  @else
-                  <td class="info-maroon">belum terisi</td>
-                  @endif
-                </tr>
-                 <tr>
-                  <td>Kartu Pelajar </td>
-                  @if($data[1]->kartu_pelajar_dir!=null)
-                  <td>sudah terupload</td>
-                  @else
-                  <td class="info-maroon">belum terisi</td>
-                  @endif
-                </tr>
-                <tr>
-                  <td colspan=2 style="text-align:right"><a href="edit-profile">Edit/Update</a></td>
-                </tr>
-              </table>
-              @endif
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+	<div class="alert alert-info">
+		<button class="close" data-dismiss="alert"></button>
+		<span>Harap segera melengkapi data tim anda, terima kasih</span>
+	</div>
 
-  </div>
+	<!-- END REGISTRATION FORM -->
+	<div id="#anggota">
+		<br>
+		<div class="row">
+			<div class="col-md-4">
+				<div class="panel panel-default">
+					<div class="panel-heading">Tim {{Auth::user()->username}}</div>
+					<div class="panel-body">
+						<table class="table table table-striped">
+							<tr>
+								<td>E-Mail </td>
+							</tr>
+							<tr>
+								<td class="pull-right">{{Auth::user()->email}}</td>
+							</tr>
+
+							<tr>
+								<td>Sekolah </td>
+							</tr>
+							<tr>
+								<td class="pull-right">{{Auth::user()->asal_sekolah}}</td>
+							</tr>
+							<tr>
+								<td>Status </td>
+							</tr>
+							<tr>
+								@if(Auth::user()->status)
+								<td class="pull-right">Terverifikasi</td>
+								@else
+								<td class="pull-right info-maroon">Belum Terverifikasi</td>
+								@endif
+							</tr>
+						</table>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-8">
+				<!--Ketua Tim-->
+				<div class="panel panel-default">
+					<div class="panel-heading">Identitas Ketua Tim :</div>
+					<div class="panel-body">
+						@if(isset($data[0]))
+						<table class="table">
+							<tr>
+								<td class="col-md-3">Nama </td>
+								@if($data[0]->nama!=null)
+								<td>{{$data[0]->nama}}</td>
+								@else
+								<td class="info-maroon">belum terisi</td>
+								@endif
+							</tr>
+							<tr>
+								<td>NIS </td>
+								@if($data[0]->nis!=null)
+								<td>{{$data[0]->nis}}</td>
+								@else
+								<td class="info-maroon">belum terisi</td>
+								@endif
+							</tr>
+							<tr>
+								<td>Tahun Masuk </td>
+								@if($data[0]->tahun_masuk!=null)
+								<td>{{$data[0]->tahun_masuk}}</td>
+								@else
+								<td class="info-maroon">belum terisi</td>
+								@endif
+							</tr>
+							<tr>
+								<td>Kontak </td>
+								@if($data[0]->handphone!=null)
+								<td>{{$data[0]->handphone}}</td>
+								@else
+								<td class="info-maroon">belum terisi</td>
+								@endif
+							</tr>
+							<tr>
+								<td>Kartu Pelajar </td>
+								@if($data[0]->kartu_pelajar_dir!=null)
+								<td>sudah terupload</td>
+								@else
+								<td class="info-maroon">belum terisi</td>
+								@endif
+							</tr>
+							<tr>
+								<td colspan=2 style="text-align:right"><a href="edit-profile">Edit/Update</a></td>
+							</tr>
+						</table>
+						@endif
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+			</div>
+			<div class="col-md-8">
+				<!--Ketua Tim-->
+				<div class="panel panel-default">
+					<div class="panel-heading">Identitas Anggota :</div>
+					<div class="panel-body">
+						<!-- Table -->
+						@if(isset($data[1]))
+						<table class="table">
+							<tr>
+								<td class="col-md-3">Nama </td>
+								@if($data[1]->nama!=null)
+								<td>{{$data[1]->nama}}</td>
+								@else
+								<td class="info-maroon">belum terisi</td>
+								@endif
+							</tr>
+							<tr>
+								<td>NIS </td>
+								@if($data[1]->nis!=null)
+								<td>{{$data[1]->nis}}</td>
+								@else
+								<td class="info-maroon">belum terisi</td>
+								@endif
+							</tr>
+							<tr>
+								<td>Tahun Masuk </td>
+								@if($data[1]->tahun_masuk!=null)
+								<td>{{$data[1]->tahun_masuk}}</td>
+								@else
+								<td class="info-maroon">belum terisi</td>
+								@endif
+							</tr>
+							<tr>
+								<td>Kontak </td>
+								@if($data[1]->handphone!=null)
+								<td>{{$data[1]->handphone}}</td>
+								@else
+								<td class="info-maroon">belum terisi</td>
+								@endif
+							</tr>
+							<tr>
+								<td>Kartu Pelajar </td>
+								@if($data[1]->kartu_pelajar_dir!=null)
+								<td>sudah terupload</td>
+								@else
+								<td class="info-maroon">belum terisi</td>
+								@endif
+							</tr>
+							<tr>
+								<td colspan=2 style="text-align:right"><a href="edit-profile">Edit/Update</a></td>
+							</tr>
+						</table>
+						@endif
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div>
 @stop
 
 @section('script')
-  @parent
-  <!-- BEGIN PAGE LEVEL PLUGINS -->
-    {{HTML::script('assets/plugins/jquery-validation/dist/jquery.validate.min.js')}}
-    {{HTML::script('assets/plugins/jquery-validation/dist/additional-methods.min.js')}}
-    {{HTML::script('assets/plugins/backstretch/jquery.backstretch.min.js')}}
-    {{HTML::script('assets/plugins/select2/select2.min.js')}}
-    <!-- END PAGE LEVEL PLUGINS -->
-    <!-- BEGIN PAGE LEVEL SCRIPTS -->
-    {{HTML::script('assets/scripts/app.js')}}
-    {{HTML::script('assets/scripts/login-soft.js')}} 
-    <!-- END PAGE LEVEL SCRIPTS --> 
-    
-    <script>
-      jQuery(document).ready(function() {     
-        App.init();
-        Login.init();
+@parent
+<!-- BEGIN PAGE LEVEL PLUGINS -->
+{{HTML::script('assets/plugins/jquery-validation/dist/jquery.validate.min.js')}}
+{{HTML::script('assets/plugins/jquery-validation/dist/additional-methods.min.js')}}
+{{HTML::script('assets/plugins/backstretch/jquery.backstretch.min.js')}}
+{{HTML::script('assets/plugins/select2/select2.min.js')}}
+{{HTML::script('assets/plugins/jquery.countdown/jquery.countdown.js')}}
+<!-- END PAGE LEVEL PLUGINS -->
+<!-- BEGIN PAGE LEVEL SCRIPTS -->
+{{HTML::script('assets/scripts/app.js')}}
+{{HTML::script('assets/scripts/login-soft.js')}} 
+<!-- END PAGE LEVEL SCRIPTS --> 
 
-            $('#paper-form input, #paper-form select').not([type="submit"]).addClass('required');
-      });
-    </script>
-    <!-- END JAVASCRIPTS -->
+<script>
+	jQuery(document).ready(function() {     
+		App.init();
+		Login.init();
 
-  @stop
+		$('#paper-form input, #paper-form select').not([type="submit"]).addClass('required');
+	});
+</script>
+
+@if(count($dataKontes)>0)
+<script>
+	$.ajax({
+		type: 'POST',
+		url: "{{URL::route('currenttime.user')}}",
+		data :{
+			time : "{{$dataKontes[0]->endtime}}"
+		},
+		success: function(data) {
+			var $date =   new Date(new Date().valueOf() +  data*1000);
+			var $clock = $('#timerUser');
+			$clock.countdown($date, function(event) {
+				$(this).html(event.strftime('<h4><kbd>%H:%M:%S</h4></kbd>'));
+			}); 
+		}
+	});
+</script>
+@endif
+<!-- END JAVASCRIPTS -->
+
+@stop
