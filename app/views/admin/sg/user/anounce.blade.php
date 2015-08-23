b84ad2dd0c8e72a58d81679a8fb391dcc9ac4420
mandes95 <mandes95@gmail.com>
2015-08-23 15:54:58 +0700

    dasd
    sad

--- app/views/admin/sg/user/anounce.blade.php
+++ app/views/admin/sg/user/anounce.blade.php
@@ -88,8 +88,7 @@
 {{HTML::script('assets/plugins/muthi/main.js')}}
 
 <script>	
-	$.fn.dataTable.Buttons.swfPath = '//cdn.datatables.net/buttons/1.0.0/swf/flashExport.swf';
- 
+	
 $(document).ready(function() {
     $('#example').DataTable( {
         dom: 'Bfrtip',
